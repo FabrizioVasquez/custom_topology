342148fe729f374ef76c745a3338e365976a0f7e
FabrizioVasquez <fabrizio.vx@gmail.com>
2021-12-18 01:16:30 -0500

    ejemplo con verificacion de host

--- custom.py
+++ custom.py
@@ -1,35 +1,29 @@
-import pdb
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
 
-class MyTopo(Topo):
-    'Ejemplo de topologia'
+class SingleSwitchTopo(Topo):
+    "Single switch connected to n hosts."
+    def build(self, n=2):
+        switch = self.addSwitch('s1')
+        # Python's range(N) generates 0..N-1
+        for h in range(n):
+            host = self.addHost('h%s' % (h + 1))
+            self.addLink(host, switch)
 
-    def __init__(self):
-        'Creando una nueva topologia'
+def simpleTest():
+    "Create and test a simple network"
+    topo = SingleSwitchTopo(n=4)
+    net = Mininet(topo)
+    net.start()
+    print( "Dumping host connections" )
+    dumpNodeConnections(net.hosts)
+    print( "Testing network connectivity" )
+    net.pingAll()
+    net.stop()
 
-        Topo.__init__(self)
-
-        S1 = self.addSwitch('S1')
-        S2 = self.addSwitch('S2')
-        S3 = self.addSwitch('S3')
-        S4 = self.addSwitch('S4')
-        S5 = self.addSwitch('S5')
-        S6 = self.addSwitch('S6')
-        H1 = self.addHost('H1')
-        H2 = self.addHost('H2')
-        H3 = self.addHost('H3')
-
-        switch_list = (S1,S2,S3,S4,S5,S6)
-
-        #add links
-
-        for index in range(len(switch_list)):
-            for index2 in range(index+1,len(switch_list)):
-                self.addLink(switch_list[index],switch_list[index2])
-        
-        self.addLink(H1,S1)
-        self.addLink(H2,S3)
-        self.addLink(H3,S6)
-    
-
-topos = {'mytopo': (lambda: MyTopo())}
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    simpleTest()
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Hito 2 Redes
 
 sudo mn --custom custom.py --topo mytopo --controller=remote,ip=127.0.0.1,port=6633
+
+
+# Branch: Develop
+
+sudo python custom.py
